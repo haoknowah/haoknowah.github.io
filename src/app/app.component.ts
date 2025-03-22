f009751caa544b30043bc384a951cb71ae664396
haoknowah <nrgman@tds.net>
2025-03-22 16:52:00 -0500

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, ElementRef } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('nav', {read:NavbarComponent, static:false}) nav !: NavbarComponent;
+  constructor()
+  {
+    
+  }
+  ngAfterViewInit(): void {
+    console.log(this.nav.num);
+    console.log(5);
+  }
   title = 'Noah Gaston';
+  num: number = 0;
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewInit{
 
-  constructor() { }
+  num: number = 2;
+
+  constructor(private el: ElementRef) { }
+  ngAfterViewInit(): void {
+    console.log(this.el.nativeElement.offsetHeight);
+  }
 
   ngOnInit(): void {
   }
