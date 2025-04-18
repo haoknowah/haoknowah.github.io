1689c730d1a5d84a762c2504e11d8956b45753e1
haoknowah <nrgman@tds.net>
2025-04-18 17:34:51 -0500

    update

--- src/app/feats/feats.component.ts
+++ src/app/feats/feats.component.ts
@@ -10,7 +10,8 @@ const FEATS: Feat[] = [
   {name: 'Event Setup', description: 'Streamlined operational processes by developing and optimizing workflows for event setups, ensuring efficient preparation and service for catering events involving up to 300+ attendees. This led to a 15% increase in overall service efficiency.'},
   {name: 'Full Stack Development', description: 'Utilize different technologies such as Angular and Spring to create and host the front end and back end of websites such as this one.'},
   {name: 'Debugging', description: 'Analyzed, debugged, and resolved over 200 technical issues reported by users, achieving a 98% resolution rate within Service Level Agreements (SLA).'},
-  {name: 'Customer Service', description: 'Enhance customer experience by quickly addressing and resolving any issues or concerns.'}
+  {name: 'Customer Service', description: 'Enhance customer experience by quickly addressing and resolving any issues or concerns.'},
+  {name: 'Meow', description: 'Likes to occasionally meow.'}
 ]
 @Component({
   selector: 'app-feats',
