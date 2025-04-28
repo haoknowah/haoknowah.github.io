56a95c69c6c5f581776b1a2e303e2e09518f5602
haoknowah <nrgman@tds.net>
2025-04-27 21:04:21 -0500

    attribute overlay fix

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -18,16 +18,26 @@ const SKILL: Skill[] = [
   {name: 'Typescript', exp: 1},
   {name: 'Microservices', exp: 1}
 ];
+export interface Attribute
+{
+  name: string;
+  value: string;
+}
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
 export class TestComponent implements OnInit{
+  ngOnInit(): void {
+  }
   displayedColumns: string[] = ['Skill', 'Years of Experience'];
   dataSource = SKILL;
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
+  attributes: Attribute[] = [
+      {name: 'Hard Work', value: '5/5'},
+      {name: 'Problem Solving', value: '4/5'},
+      {name: 'Dependability', value: '5/5'},
+      {name: 'Obedience', value: '5/5'},
+      {name: 'Humor', value: '4/5'}
+  ];
 }
